ebb7d870b62611e84d9802e5b662e2cbe52a9b08
ameetpokhrel02 <pokhrelameet12@gmail.com>
2025-06-05 23:00:13 +0545

    githublinkadded

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -59,7 +59,7 @@ const Footer = () => {
               <MuiLink href="#" sx={{ color: '#555', mr: 2, '&:hover': { color: 'primary.main' } }}><Twitter /></MuiLink>
               <MuiLink href="#" sx={{ color: '#555', mr: 2, '&:hover': { color: 'primary.main' } }}><Facebook /></MuiLink>
               <MuiLink href="#" sx={{ color: '#555', mr: 2, '&:hover': { color: 'primary.main' } }}><Instagram /></MuiLink>
-              <MuiLink href="#" sx={{ color: '#555', mr: 2, '&:hover': { color: 'primary.main' } }}><GitHub /></MuiLink>
+              <MuiLink href="https://github.com/ameetpokhrel02" sx={{ color: '#555', mr: 2, '&:hover': { color: 'primary.main' } }}><GitHub /></MuiLink>
               <MuiLink href="#" sx={{ color: '#555', '&:hover': { color: 'primary.main' } }}><LinkedIn /></MuiLink>
             </Box>
           </Box>
