0d4112cb9759c794acb39efb936560ef3edcb02c
ameetpokhrel02 <pokhrelameet12@gmail.com>
2025-06-27 11:28:47 +0545

    dfds

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,1 +1,12 @@
- 
+from rest_framework import serializers
+from .models import Product, Blog
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__' 
