62903b501842eda11bac50831f9feead33e8d372
your_github_username <your_github_email>
2025-07-02 16:11:49 +0545

    d

--- accounts/__init__.py
+++ accounts/__init__.py
@@ -6,4 +6,5 @@ class CustomUser(AbstractUser):
         ('customer', 'Customer'),
         ('admin', 'Admin'),
     )
+    is_active = models.BooleanField(default=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='customer')
