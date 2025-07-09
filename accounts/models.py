c6304f8bcd288431fc8a11b37746279924fb7407
your_github_username <your_github_email>
2025-07-09 14:32:18 +0545

    addedfff

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('admin', 'Admin'),
+    )
+    is_active = models.BooleanField(default=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='customer')
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
