6b5ea5f10688c15ea3bd6e57663e56e6330e3f61
your_github_username <your_github_email>
2025-07-11 15:23:31 +0545

    kg

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!!
 SECRET_KEY = 'django-insecure-bosp#dy6r*wj4vi@dp-z7lp#6&$pdx=-bw-ln=#*8d@kgczxzi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
