e345159fff666d5f5d28aa1b63abecbb692e24ad
your_github_username <your_github_email>
2025-07-15 11:15:39 +0545

    modified the models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here to use.
+# Create your models here to uses.
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
